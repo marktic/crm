ea8743ea14d02931a6c25f6c0b5f3ee7b4f7dcda
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-02-08 23:41:58 +0200

    Add translation and form handling traits to CrmRecordManager

--- src/AbstractBase/Models/CrmRecordManager.php
+++ src/AbstractBase/Models/CrmRecordManager.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Marktic\Crm\AbstractBase\Models;
 
+use ByTIC\Records\Behaviors\HasForms\HasFormsRecordsTrait;
+use Nip\I18n\Translatable\HasTranslations;
 use Nip\Records\RecordManager;
 
 /**
@@ -13,5 +15,9 @@ use Nip\Records\RecordManager;
 abstract class CrmRecordManager extends RecordManager
 {
     use \ByTIC\DataObjects\Behaviors\Timestampable\TimestampableManagerTrait;
+
+    use HasTranslations;
+    use HasFormsRecordsTrait;
+
     use HasDatabaseConnectionTrait;
 }
