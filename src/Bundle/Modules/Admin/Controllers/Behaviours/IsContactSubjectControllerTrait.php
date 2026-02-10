d1bd69c4283b14fad46011ff81d99d9a8c9f13ec
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-02-10 09:53:38 +0200

    Refactor IsContactSubjectControllerTrait to include AbstractCrmControllerTrait; update payloadSetContacts method to use tenant from context

--- src/Bundle/Modules/Admin/Controllers/Behaviours/IsContactSubjectControllerTrait.php
+++ src/Bundle/Modules/Admin/Controllers/Behaviours/IsContactSubjectControllerTrait.php
@@ -2,12 +2,16 @@
 
 namespace Marktic\Crm\Bundle\Modules\Admin\Controllers\Behaviours;
 
+use Marktic\Crm\Bundle\Modules\Admin\Controllers\AbstractCrmControllerTrait;
 use Marktic\Crm\Contacts\Actions\Find\FindCrmContacts;
 
 trait IsContactSubjectControllerTrait
 {
+    use AbstractCrmControllerTrait;
+
     protected function payloadSetContacts($subject, $tenant = null)
     {
+        $tenant = $tenant ?? $this->getCrmTenant();
         $contacts = FindCrmContacts::for($subject)->withTenant($tenant)->fetch();
         $this->payload()->set('crm_contacts', $contacts);
     }
